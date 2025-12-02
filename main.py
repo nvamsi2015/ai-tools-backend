055bf994701643439668e90040e26998f8c9eea1
vamsi <nvamsi2015@gmail.com>
2025-12-02 19:00:36 +0530

    added fakestore api

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+import httpx
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
+
+@app.get('/fakestore')
+async def get_dashboard():
+    print('new request received')
+    async with httpx.AsyncClient() as client:
+        response = await client.get('https://fakestoreapi.com/products')
+        print(response.status_code)
+        data = response.json()
+    return data
